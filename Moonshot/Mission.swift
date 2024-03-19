1902c1069475fac000576fb17b5d8b0f92911876
koonzwg <koonzwg@gmail.com>
2024-03-19 10:26:30 -0500

    Day 41 complete

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -18,7 +18,7 @@ struct Mission: Codable, Identifiable {
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
@@ -28,5 +28,9 @@ struct Mission: Codable, Identifiable {
     
     var image: String {
         "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
